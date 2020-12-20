2a465f43dbec171cd0129fde9ed451a07d4bc107
black8cherry <art8evd@gmail.com>
2020-12-20 17:39:57 +0300

    calendar creation, updating, deletion updated

--- backend-server/src/main/java/com/unn/service/ICalendarService.java
+++ backend-server/src/main/java/com/unn/service/ICalendarService.java
@@ -3,14 +3,13 @@ package com.unn.service;
 import java.util.Optional;
 
 import com.unn.model.Calendar;
-import com.unn.model.Calendar;
 
 public interface ICalendarService {
-  Optional<Calendar> createCalendarByDoctorID(Long doctorId, int startTime, int endTime);
+    Optional<Calendar> createCalendarByDoctorID(Long doctorId, int startTime, int endTime);
 
-  Optional<Calendar> findCalendar(Long calendarId);
-  
-  Optional<Calendar> modifyCalendar(Long calendarId);
+    Optional<Calendar> findCalendar(Long calendarId);
 
-  Optional<Calendar> deleteCalendar(Long calendarId);
+    void modifyCalendar();
+
+    Optional<Calendar> deleteCalendar(Long calendarId);
 }
--- backend-server/src/main/java/com/unn/service/impl/CalendarService.java
+++ backend-server/src/main/java/com/unn/service/impl/CalendarService.java
@@ -1,13 +1,10 @@
 package com.unn.service.impl;
 
-import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.temporal.ChronoField;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import com.unn.model.Appointment;
 import com.unn.model.Calendar;
@@ -17,86 +14,117 @@ import com.unn.repository.CalendarRepo;
 import com.unn.repository.DoctorRepo;
 import com.unn.service.ICalendarService;
 
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
+@EnableScheduling
 public class CalendarService implements ICalendarService {
-  private final CalendarRepo calendarRepo;
-  private final DoctorRepo doctorRepo;
-  private final AppointmentRepo appointmentRepo;
+    private final CalendarRepo calendarRepo;
+    private final DoctorRepo doctorRepo;
+    private final AppointmentRepo appointmentRepo;
 
-  private final ValidationService validationService;
-  private final AppointmentService appointmentService;
+    private final ValidationService validationService;
+    private final AppointmentService appointmentService;
 
-  @Override
-  public Optional<Calendar> createCalendarByDoctorID(Long doctorId, int startTime, int endTime) {
-    Calendar calendar = new Calendar();
+    @Override
+    public Optional<Calendar> createCalendarByDoctorID(Long doctorId, int startTime, int endTime) {
+        Calendar calendar = new Calendar();
 
-    if (validationService.validateWorkTime(startTime, endTime)) {
-      calendar.setStartTime(startTime);
-      calendar.setEndTime(endTime);
+        if (validationService.validateWorkTime(startTime, endTime)) {
+            calendar.setStartTime(startTime);
+            calendar.setEndTime(endTime);
 
-      Optional<Doctor> doctor = doctorRepo.findById(doctorId);
-      calendar.setDoctor(doctor.get());
+            Optional<Doctor> doctor = doctorRepo.findById(doctorId);
+            calendar.setDoctor(doctor.get());
 
-      Map<Long, Appointment> appointments;
+            Map<Long, Appointment> appointments;
 
-      Date currentDate = new Date();
-      for (int day = currentDate.getDay(); day < day + 14; day++) {
-        for (int hour = startTime; hour < endTime; hour++) {
-          Date appointmentDate = new Date(currentDate.getYear(), day, hour);
-          Appointment newAppointment = new Appointment(doctorId, appointmentDate);
-          appointmentRepo.save(newAppointment);
-          appointments.put(newAppointment.getId(), newAppointment);
+            LocalDate currentDate = LocalDate.now();
+            LocalDate endDate = currentDate.plusDays(14); // schedule for two weeks
+            for (LocalDate startDate = currentDate; startDate.isBefore(endDate); startDate = startDate.plusDays(1)) {
+                if (startDate.getDayOfWeek().getValue() != 6 && startDate.getDayOfWeek().getValue() != 7) {
+                    for (int startHour = startTime; startHour < endTime; startHour++) {
+                        Date appointmentDate = java.sql.Date.valueOf(startDate);
+                        Appointment newAppointment = new Appointment(doctorId, appointmentDate.setHours(startHour));
+                        appointmentRepo.save(newAppointment);
+                        appointments.put(newAppointment.getId(), newAppointment);
+                    }
+                }
+            }
+
+            calendar.setAppointments(appointments);
         }
-      }
-
-      calendar.setAppointments(appointments);      
-    }
-    calendarRepo.save(calendar);
-    return Optional.of(calendar);
-  }
-
-  @Override
-  public Optional<Calendar> findCalendar(Long calendarId) {
-    return calendarRepo.findById(calendarId);
-  }
-
-  @Override
-  public Optional<Calendar> modifyCalendar(Long calendarId) {
-    Optional<Calendar> calendar = calendarRepo.findById(calendarId);
-
-    if (calendar.isPresent()) {
-      Doctor doctor = calendar.get().getDoctor();
-      Long doctorId = doctor.getId();
-
-      Map<Long, Appointment> apm;
-      Optional<List<Appointment>> appointments = appointmentRepo.findAllByDoctorId(doctorId);
-
-      if (appointments.isPresent()) {
-        apm = appointments.get().stream().collect(
-              Collectors.toMap(Appointment::getId, appointment -> appointment)
-            );
-
-        calendar.get().setAppointments(apm);
-        calendarRepo.save(calendar.get());
-      }
+        calendarRepo.save(calendar);
+        return Optional.of(calendar);
     }
 
-    return calendar;
-  }
-
-  @Override
-  public Optional<Calendar> deleteCalendar(Long calendarId) {
-    Optional<Calendar> calendar = calendarRepo.findById(calendarId);
-    
-    if (calendar.isPresent()) {
-      calendarRepo.delete(calendar.get());
+    @Override
+    public Optional<Calendar> findCalendar(Long calendarId) {
+        return calendarRepo.findById(calendarId);
     }
-    
-    return calendar;
-  }
+
+    @Override
+    @Scheduled(cron = "0 0 0 * * 0")
+    public void modifyCalendar() {
+        LocalDate currentDate = LocalDate.now();
+        List<Calendar> allCalendars = calendarRepo.findAll();
+        allCalendars.forEach(
+            calendar -> {
+                // deleting last week
+                Date newStartDate = java.sql.Date.valueOf(currentDate);
+                calendar
+                    .getAppointments()
+                    .forEach(
+                        (appointmentId, appointment) -> {
+                            if (appointment.getDate().before(newStartDate)) {
+                                appointmentRepo.deleteById(appointmentId);
+                                calendar.getAppointments().remove(appointmentId);
+                            }
+                        }
+                    );
+                // filling next week
+                for (
+                    LocalDate nextWeekStartDate = currentDate.plusDays(7);
+                    nextWeekStartDate.isBefore(currentDate.plusDays(14));
+                    nextWeekStartDate = nextWeekStartDate.plusDays(1)
+                ) {
+                    if (
+                        nextWeekStartDate.getDayOfWeek().getValue() != 6 &&
+                        nextWeekStartDate.getDayOfWeek().getValue() != 7
+                    ) {
+                        for (int startHour = calendar.getStartTime(); startHour < calendar.getEndTime(); startHour++) {
+                            Date appointmentDate = java.sql.Date.valueOf(nextWeekStartDate);
+                            Appointment newAppointment = new Appointment(
+                                calendar.getDoctor().getId(),
+                                appointmentDate.setHours(startHour)
+                            );
+                            appointmentRepo.save(newAppointment);
+                            calendar.getAppointments().put(newAppointment.getId(), newAppointment);
+                        }
+                    }
+                }
+                calendarRepo.save(calendar);
+            }
+        );
+    }
+
+    @Override
+    public Optional<Calendar> deleteCalendar(Long calendarId) {
+        Optional<Calendar> calendar = calendarRepo.findById(calendarId);
+
+        if (calendar.isPresent()) {
+            calendarRepo.delete(calendar.get());
+            calendar
+                .get()
+                .getAppointments()
+                .forEach((appointmentId, appointment) -> appointmentRepo.deleteById(appointmentId));
+        }
+
+        return calendar;
+    }
 }
