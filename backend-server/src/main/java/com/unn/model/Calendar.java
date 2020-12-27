02407c969dc5e457db9c80a6acce1d65e6f440d4
black8cherry <art8evd@gmail.com>
2020-12-27 20:49:20 +0300

    foreign key updated

--- backend-server/src/main/java/com/unn/model/Calendar.java
+++ backend-server/src/main/java/com/unn/model/Calendar.java
@@ -1,6 +1,7 @@
 package com.unn.model;
 
 import java.util.Map;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -9,9 +10,14 @@ import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -27,6 +33,7 @@ public class Calendar {
 
     @OneToOne
     @JoinColumn(name = "doctor_id")
+    @OnDelete(action = OnDeleteAction.CASCADE)
     @JsonIgnore
     private Doctor doctor;
 
